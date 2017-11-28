22e53fc5a1d38148286f739cba664aed25189d3e
Julio Duarte <julio.cb.duarte@gmail.com>
2017-11-28 21:42:04 -0200

    Local Notifications

--- src/providers/favorite/favorite.ts
+++ src/providers/favorite/favorite.ts
@@ -4,6 +4,7 @@ import { Dish } from '../../shared/dish';
 import { Observable } from 'rxjs/Observable';
 import { DishProvider } from '../dish/dish';
 import { Storage } from '@ionic/storage';
+import { LocalNotifications } from '@ionic-native/local-notifications';
 
 /*
   Generated class for the FavoriteProvider provider.
@@ -18,7 +19,8 @@ export class FavoriteProvider {
 
   constructor(public http: Http,
     private dishservice: DishProvider,
-    private storage: Storage) {
+    private storage: Storage,
+    private localNotifications: LocalNotifications) {
     console.log('Hello FavoriteProvider Provider');
     this.favorites = [];
 
@@ -36,6 +38,10 @@ export class FavoriteProvider {
     {
       this.favorites.push(id);
       this.storage.set('favorites', this.favorites);
+      this.localNotifications.schedule({
+        id: id,
+        text: 'Dish ' +  id + ' added as favorite successfully'
+      });
     }
     return true;
   }
